cb47390479d59440e83e4e5da70dbb0a654a7c33
Lebedenko Tatiana <lebedenko_tatiana@mail.ru>
2016-08-12 13:58:35 +0300

    second commt

--- js/sqript.js
+++ js/sqript.js
@@ -6,7 +6,8 @@ element.innerHTML ='Тест по программированию';
 var parentElement = document.body;
 parentElement.appendChild(element);
 
-element = document.createElement('div');
+element = document.createElement('form');
+element.name = 'TestForm'
 element.className = 'col-md-6 col-md-offset-3';
 parentElement.appendChild(element);
 
@@ -16,46 +17,34 @@ for (var n = 1; n < 4; n++) {
   element = document.createElement('h4');
   element.className = 'question';
   element.innerHTML = n + '. Вопрос № ' + n;
-  parentElement = document.querySelector('.col-md-6');
+  parentElement = document.querySelector('form');
   parentElement.appendChild(element);
 
   for (var i = 1; i < 4; i++) {
-
-    element = document.createElement('div');
-    element.className = 'checkbox';
-    parentElement = document.querySelector('.col-md-6');
+    
+    element = document.createElement('label'); 
+    element.className = 'question__label';
+    element.style.display = 'block';
+    parentElement = document.querySelector('form');
     parentElement.appendChild(element);
 
     element = document.createElement('input');
     element.type = 'checkbox';
     element.id = 'check_id' + n + i;
     element.className = 'question__checkbox';
-    parentElement = document.querySelectorAll('.checkbox');
+    parentElement = document.querySelectorAll('label');
     parentElement[parentElement.length-1].appendChild(element);
 
-    element = document.createElement('label'); 
-    element.htmlFor = 'check_id' + n + i;
-    element.innerHTML = 'Вариант ответа № ' + i;
-    parentElement[parentElement.length-1].appendChild(element);
+    parentElement[parentElement.length-1].appendChild(document.createTextNode('Вариант ответа № ' + i));
 
   }
 
 }
 
-parentElement = document.querySelector('.col-md-6');
-element = document.createElement('div');
-element.className = 'text-center';
-parentElement.appendChild(element);
-
+parentElement = document.querySelector('form');
 element = document.createElement('input');
 element.type = 'submit';
-element.className = 'question__submit';
+element.className = 'question__submit center-block';
 element.value = 'Проверить мои результаты';
-parentElement = document.querySelectorAll('.text-center');
-parentElement[parentElement.length-1].appendChild(element);
-
-
-
-
-  
-
+element.style.display = 'block';
+parentElement.appendChild(element);
